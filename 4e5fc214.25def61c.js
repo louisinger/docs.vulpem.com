0b8ffd4ae9bc402aa7bc9fd654c8ae68e0fddbfb
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-03-04 11:10:39 +0100

    Deploy website - based on e6137fdca48b9b954366eae91f52f87e26f456e7

---
+++ 4e5fc214.25def61c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{80:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/docs.vulpem.com/blog/welcome"},{"title":"Hello","permalink":"/docs.vulpem.com/blog/hello-world"},{"title":"Hola","permalink":"/docs.vulpem.com/blog/hola"}]}')}}]);
